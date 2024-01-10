18b122d652abb360cfc24e4121d1a7fd8624da2b
hongunner <shs6626@naver.com>
2024-01-11 00:08:52 +0900

    Caculator

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# react
+change commit name
---
+++ src/App.js
@@ -0,0 +1,30 @@
+const { Component } = require("react");
+
+class App extends Component {
+  render() {
+    return (
+      <main className="main-container">
+        <h1>예산 게산기</h1>
+        <div
+          style={{ width: "100%", backgroundColor: "white", padding: "1rem" }}
+        >
+          Expens Form
+        </div>
+        <div
+          style={{ width: "100%", backgroundColor: "white", padding: "1rem" }}
+        >
+          Expens List
+        </div>
+
+        <div style={{ display: "flex", justifyContent: "end", margin: "1rem" }}>
+          <p style={{ fontSize: "2rem" }}>
+            Total:
+            <span>원</span>
+          </p>
+        </div>
+      </main>
+    );
+  }
+}
+
+export default App;
